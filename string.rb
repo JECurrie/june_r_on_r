0fd37d45ea7f0243b5369e23ad442212edc6c1cc
Joan Currie <jecurrie8@gmail.com>
2018-06-13 18:06:13 +0000

    example of string

---
+++ string.rb
@@ -0,0 +1,11 @@
+##w3e5
+#a string s = 'key=value',
+s = 'key=value'
+puts s
+
+##a = "hello "
+##a << "world"   #=> "hello world"
+##puts a.concat(25)   #=> "hello world!"
+
+puts s[0..2]
+puts s[4..8]
---
+++ time.rb
@@ -0,0 +1,10 @@
+#w7e4:
+#                 file_name
+def last_modified()
+#file was last modified 125.873605469919 days ago.
+
+  puts Time.at(0)
+
+
+end
+last_modified()
